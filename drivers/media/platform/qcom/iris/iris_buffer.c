2d5a583dc33b971fd6df1224b08c0db2e49f18ae
Dikshita Agarwal <quic_dikshita@quicinc.com>
2025-02-21 16:05:41 +0530

    media: iris: avoid sending LAST flag multiple times
    Add a flag to check if LAST flag is sent by driver, so that it is send
    only once for a sequence.
    
    Signed-off-by: Dikshita Agarwal <quic_dikshita@quicinc.com>

--- drivers/media/platform/qcom/iris/iris_buffer.c
+++ drivers/media/platform/qcom/iris/iris_buffer.c
@@ -630,6 +630,7 @@ int iris_vb2_buffer_done(struct iris_inst *inst, struct iris_buffer *buf)
 			v4l2_event_queue_fh(&inst->fh, &ev);
 			v4l2_m2m_mark_stopped(m2m_ctx);
 		}
+		inst->last_buffer_dequeued = true;
 	}
 	vb2->timestamp = buf->timestamp;
 	v4l2_m2m_buf_done(vbuf, state);
